77e373844cc27e5b9ad0b48aa7fec7617f593f57
Lucas <lucas.santos@dotgroup.com.br>
2025-10-14 22:22:37 -0300

    Adicionando Stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,31 +1,52 @@
-import http from 'k6/http'
-import { check, sleep } from 'k6'
+import http from 'k6/http';
+import { check, sleep } from 'k6';
 
 export const options = {
-    vus: 10,
-    duration: '30s',
-    thresholds: {
-        'http_req_failed': ['rate<0.01'],
-        'http_req_duration': ['p(90)<1000', 'max<3000']
+  stages: [
+    {
+      duration: '10s',
+      target: 10,
+    },
+    {
+      duration: '20s',
+      target: 10,
+    },
+    {
+      duration: '10s',
+      target: 30,
+    },
+    {
+        duration: '20s',
+        target: 30
+    },
+    {
+        duration: '20s',
+        target:0
     }
-}
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(90)<1000', 'max<3000'],
+  },
+};
 
-export default function (){
-    const url = 'http://localhost:3000/users/login'
-    const payload = JSON.stringify({
-        username: 'julio',
-        password: '123456'
-    })
+export default function () {
+  const url = 'http://localhost:3000/users/login';
+  const payload = JSON.stringify({
+    username: 'julio',
+    password: '123456',
+  });
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    const resposta = http.post(url, payload, params)
-    check(resposta, {
-        "Validar que o status é igual a 200": (r) => r.status == 200,
-        "Validar que o token é uma string": (r) => typeof(r.json().token) == 'string'
-    }) 
-    sleep(1)
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const resposta = http.post(url, payload, params);
+  check(resposta, {
+    'Validar que o status é igual a 200': (r) => r.status == 200,
+    'Validar que o token é uma string': (r) =>
+      typeof r.json().token == 'string',
+  });
+  sleep(1);
 }
